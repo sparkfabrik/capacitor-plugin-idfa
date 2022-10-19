737e893724182fc14473c6351483ec1d4f8dfbde
jgessinger@aeins.de <jgessinger@aeins.de>
2022-10-19 14:54:14 +0200

    Removed deprecated constructor from web implementation

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { IdfaPlugin } from './definitions';
 
-export * from './definitions';
-export * from './web';
-
 const Idfa = registerPlugin<IdfaPlugin>('Idfa', {
   web: () => import('./web').then(m => new m.IdfaWeb()),
 });
